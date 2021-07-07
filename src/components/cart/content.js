91596d41d54cbe45360a4450f10815ed32c9462e
john <johnbabu021@gmail.com>
2021-07-07 10:18:49 +0530

    error in cart is resolved

--- src/components/cart/content.js
+++ src/components/cart/content.js
@@ -8,9 +8,9 @@ export default function Content() {
     const [cart, setCart] = useState(null)
 
     const { user: { uid: userId = '' } } = useContext(userContext)
-    useEffect( () => {
+    useEffect(async () => {
 
-        const [result] =  getUserCartItems(userId)
+        const [result] = await getUserCartItems(userId)
         setCart(result);
         // here cart will be empty usestate
 
